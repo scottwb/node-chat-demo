cb5a47f2d70788ad0c813ac8012682443017b2b3
Scott W. Bradley <scottwb@scottwb.net>
2012-02-20 12:39:42 -0800

    Implement basic chat-room client/server app with Socket.io.

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express    = require('express');
+var RedisStore = require('connect-redis')(express);
+var routes     = require('./routes');
 
 var app = module.exports = express.createServer();
+var io  = require('socket.io').listen(app);
 
 // Configuration
 
 app.configure(function(){
-  var RedisStore = require('connect-redis')(express);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -36,3 +37,24 @@ app.get('/', routes.index);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+io.sockets.on('connection', function(socket) {
+
+    socket.on('set nickname', function(name) {
+        socket.set('nickname', name, function() {
+            socket.emit('logged in', name);
+            console.log(name + ' just joined the conversation');
+            io.sockets.emit('joined', name);
+        });
+    });
+
+    socket.on('message', function(msg) {
+        socket.get('nickname', function(err, name) {
+            console.log('Chat message received from ', name);
+            io.sockets.emit('message', {
+                username : name,
+                message  : msg
+            });
+        });
+    });
+});
---
+++ public/javascripts/chat.js
@@ -0,0 +1,36 @@
+var socket = io.connect('http://localhost');
+
+$(function() {
+    $('form#login').bind('submit', function() {
+        var name = $('input#nickname').val();
+        socket.emit('set nickname', name);
+    });
+
+    $('form#chat').bind('submit', function() {
+        var msg = $('input#message').val();
+        socket.emit('message', msg);
+    });
+});
+
+socket.on('logged in', function(name) {
+    console.log('Logged in as: ', name);
+    $('form#login').hide();
+    $('.chat').show();
+});
+
+socket.on('joined', function(name) {
+    $('.chat #transcript table').append(
+        "<tr class='joined'><td colspan='2'>"   +
+        name + " just joined the conversation." +
+        "</td></tr>"
+    );
+});
+
+socket.on('message', function(data) {
+    $('.chat #transcript table').append(
+        "<tr class='message'>"              +
+        "  <th>" + data.username + ":</th>" +
+        "  <td>" + data.message + "</td>"   +
+        "</tr>"
+    );
+});
