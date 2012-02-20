55b209222b88a897c88091eadf0e54e8421de9a3
Scott W. Bradley <scottwb@scottwb.net>
2012-02-20 07:38:35 -0800

    Use RedisStore for sessions

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
+  var RedisStore = require('connect-redis')(express);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret:"ASecretKey", store:new RedisStore}));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
